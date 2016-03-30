ba7e8f8bbeaa293215eeb3432e1d2574dd214879
nolin897 <nolin897@gmail.com>
2016-03-30 00:36:11 -0400

    Navbar Changes and styling changes Wed 03-29-16
    Navbar Changes and styling changes Wed 03-29-16

--- js/script.js
+++ js/script.js
@@ -1,30 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function(event){
 
-	
-	// $('section.epigraph > h2').mouseenter(function(event){
-	// 	console.log("Hovering Over");
-	// 	$('section.epigraph h2').hide();
-	// }).mouseleave(function(event){
-	// 	console.log("Hovering Out");
-	// 	$('section.epigraph span').show();
-	// });
-
-
-	$( "section.epigraph" ).mouseenter(function(event) {
-		console.log(mouseenter);
-	    $("section.epigraph h2").hide(event).fadeOut( 500 );
-	    console.log(hide).log(fadeOut);
-	    $("section.epigraph span").show(event).fadeIn( 500 );
-		consol.log(show).log(fadeIn);
+	$("section.epigraph").mouseenter(function(event){
+		// console.log(mouseenter);
+	    $("section.epigraph h2#greek").hide(event).fadeOut(500);
+	    // console.log(hide).log(fadeOut);
+	    $("section.epigraph h2#translation").show(event).fadeIn(500);
+		// consol.log(show).log(fadeIn);
 	});
 
-	$( "section.epigraph" ).mouseleave(function(event) {
-		console.log(mouseleave);
-	    $("section.epigraph h2").show(event).fadeOut( 500 );
-	    console.log(show).log(fadeOut);
-	    $("section.epigraph span").hide(event).fadeIn( 500 );
-	    console.log(hide).log(fadeIn);
+	$("section.epigraph").mouseleave(function(event){
+		// console.log(mouseleave);
+	    $("section.epigraph h2#greek").show(event).fadeOut(500);
+	    // console.log(show).log(fadeOut);
+	    $("section.epigraph h2#translation").hide(event).fadeIn(500);
+	    // console.log(hide).log(fadeIn);
 	});
 
+});	
 
-});
+
