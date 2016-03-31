25b07f1082ff1e7d03ed1178b7ab0c46e544a96a
nolin897 <nolin897@gmail.com>
2016-03-30 23:45:59 -0400

    Wednesday 03-30-16 Changes
    Wednesday 03-30-16 Changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 $(document).ready(function(event){
 
-	$("section.epigraph").mouseenter(function(event){
-		// console.log(mouseenter);
-	    $("section.epigraph h2#greek").hide(event).fadeOut(500);
-	    // console.log(hide).log(fadeOut);
-	    $("section.epigraph h2#translation").show(event).fadeIn(500);
-		// consol.log(show).log(fadeIn);
-	});
+	$("section.epigraph").mouseenter(function(){
+	    $("section.epigraph > h2#greek").hide(event).fadeOut(3000);
+	    $("section.epigraph > h2#translation").show(event).fadeIn(3000);
+		});
 
-	$("section.epigraph").mouseleave(function(event){
-		// console.log(mouseleave);
-	    $("section.epigraph h2#greek").show(event).fadeOut(500);
-	    // console.log(show).log(fadeOut);
-	    $("section.epigraph h2#translation").hide(event).fadeIn(500);
-	    // console.log(hide).log(fadeIn);
+	$("section.epigraph").mouseleave(function(){
+	    $("section.epigraph > h2#translation").hide(event).fadeOut(3000);
+	    $("section.epigraph > h2#greek").show(event).fadeIn(3000);
+
+	return false;
+
 	});
 
 });	
